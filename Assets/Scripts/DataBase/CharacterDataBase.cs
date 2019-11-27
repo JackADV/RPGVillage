d5530e8bf8858e4b3cc9f75a43a6a800cf7be352
Jack <47583484+JackADV@users.noreply.github.com>
2019-11-27 16:10:57 +1100

    Update

---
+++ Assets/Scripts/DataBase/CharacterDataBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterDataBase : MonoBehaviour
+{
+    public int id, level, strength, agility, intelligence, health;
+    public string characterName;
+
+
+    IEnumerator CharacterInformation(int charId, int charLevel, int charStrength, int charAgility, int charIntelligene, int charHealth, string charName)
+    {
+        string characterInformationURL = "http://localhost/loginproject/weapons.php";
+        WWWForm form = new WWWForm();
+        form.AddField("int", charId);
+        form.AddField("int", charLevel);
+        form.AddField("int", charStrength);
+        form.AddField("int", charAgility);
+        form.AddField("int", charIntelligene);
+        form.AddField("int", charHealth);
+        form.AddField("string", charName);
+        UnityWebRequest webRequest = UnityWebRequest.Post(characterInformationURL, form);
+        yield return webRequest.SendWebRequest();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            StartCoroutine(CharacterInformation(id,level,strength,agility,intelligence,health,characterName));
+        }
+    }
+}
---
+++ Assets/Scripts/DataBase/WeaponDataBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeaponDataBase : MonoBehaviour
+{
+    public int id;
+    public int stats;
+    public string type;
+
+    IEnumerator WeaponInformationSet(int weaponStats, string weaponType, int idNumber)
+    {
+        string weaponInformationURL = "http://localhost/loginproject/weapons.php";
+        WWWForm form = new WWWForm();
+        form.AddField("type", weaponType);
+        form.AddField("id", idNumber);
+        form.AddField("stats", weaponStats);
+        UnityWebRequest webRequest = UnityWebRequest.Post(weaponInformationURL, form);
+        yield return webRequest.SendWebRequest();
+    }
+    IEnumerator WeaponInformationGet(int id)
+    {
+        string weaponInformationURL = "http://localhost/loginproject/weaponsGet.php";
+        WWWForm form = new WWWForm();
+        form.AddField("id", id);
+        UnityWebRequest webRequest = UnityWebRequest.Post(weaponInformationURL, form);
+        yield return webRequest.SendWebRequest();
+        //split and parse string to locations
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            StartCoroutine(WeaponInformationSet(stats, type,id));
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            StartCoroutine(WeaponInformationGet(id));
+        }
+    }
+}
